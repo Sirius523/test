3e56c1e02220307556f9a0bbfc3b6dd9ffb379e7
Danil <sirius0218@mail.ru>
2022-12-22 13:10:19 +0500

    first

---
+++ script.js
@@ -0,0 +1,79 @@
+'use strict';
+//Задание 1
+let a1 = 7 + 2,
+a2 = 7 - 3,
+a3 = 2 * 4,
+a4 = 4 / 2;
+console.log(`1)${a1}, ${a2}, ${a3}, ${a4}`)
+//Задание 2
+let height = 23,
+width = 10,
+s = height * width;
+console.log(`2)${s}`)
+//Задание 3
+let b1 = 5 % 3,
+b2 = 3 % 5,
+b3 = 5 + '3',
+b4 = '5' - 3,
+b5 = 75 + 'кг';
+console.log(`3)${b1}, ${b2}, ${b3}, ${b4}, ${b5}`)
+//Задание 4
+let heightC = 10,
+dC = 4,
+v = heightC * dC;
+console.log(`4)${v}`)
+//Задание 5
+let a = 10,
+b = 2,
+sum = a + b,
+diff = a - b,
+multi = a * b,
+div = a / b;
+console.log(`5)${a+b}, ${a-b}, ${a*b}, ${a/b}`)
+//Задание 6
+let c = 15,
+d = 2,
+sum6 = c + d,
+result6 = sum6;
+console.log(`6)${result6}`)
+//Задание 7
+let a7 = 10,
+b7 = 2,
+c7 = 5;
+console.log(`7)${a7+b7+c7}`)
+//Задание 8
+let a8 = 17,
+b8 = 10,
+c8 = a8 - b8,
+d8 = 7,
+result8 = c8 + d8;
+console.log(`8)${result8}`)
+//Задание 9
+let name9 = "Danil и Artem";
+console.log(`9)Привет, ${name9}`)
+//Задание 10
+let age10 = 10;
+console.log(`10)Мне ${age10} лет!`)
+//Задание 11
+let str11 = "abcde";
+console.log(`11)${str11[0]}, ${str11[2]}, ${str11[4]}`)
+//Задание 12
+let sec = 1,
+min = sec * 60,
+hour = min * 60,
+day = hour * 24,
+month = day * 30;
+console.log(`12)${hour}, ${day}, ${month}`)
+//Задание 13
+let num13 = 47;
+num13 += 7,
+num13 -= 18,
+num13 *= 10,
+num13 /= 15;
+console.log(`13)${num13}`)
+//Задание 14
+let num14 = 10;
+num14 = ++num14,
+num14 = ++num14,
+num14 = --num14;
+console.log(`14)${num14}`)
